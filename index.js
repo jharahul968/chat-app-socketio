b9877cf6947351edaefa65c09221000d235acedd
jharahul968 <jharahul968@gmail.com>
2023-06-26 12:45:03 +0545

    functionality for nickname done

--- index.js
+++ index.js
@@ -8,12 +8,21 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  socket.on("chat message", msg => {
-    io.emit("chat message", msg);
+  socket.nickname = "Guest";
+  io.emit("new connection", "a new user connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", socket.nickname + " : " + msg);
   });
-  socket.on("disconnect", ()=>{
-    io.emit("disconnected", "a user disconnected")
-  })
+  socket.on("disconnect", () => {
+    io.emit("disconnected", "a user disconnected");
+  });
+  socket.on("nickname changed", (msg) => {
+    socket.nickname = msg;
+    io.emit("nickname_change_notif", `Nickname changed to ${msg}`);
+  });
+  socket.on("typing", () => {
+    io.emit("typing", "typing");
+  });
 });
 
 http.listen(3000, () => {
